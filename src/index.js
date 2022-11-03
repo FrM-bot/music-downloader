eb899ba04c4934f10db621185c5f589d1d58dbb4
Franco M <damianmaciel0@gmail.com>
2022-11-03 18:46:30 -0300

    fix replaceAll

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ app.post('/download', async (req, res) => {
         }
         ytdl.downloadFromInfo(info, {
             filter: (format) => format.container === 'webm' && format.hasAudio,
-        }).pipe(fs.createWriteStream(path.join(DEST_DOWNLOADS, details.title.replaceAll(' ', '_') + EXTENSION_FILE)))
+        }).pipe(fs.createWriteStream(path.join(DEST_DOWNLOADS, info.videoDetails.title.split(' ').join('_') + EXTENSION_FILE)))
         res.status(200).json(details)
     } catch (error) {
         console.error(error)
@@ -90,7 +90,7 @@ app.post('/download', async (req, res) => {
 
 app.post('/clear', async (req, res) => {
     try {
-        const name = req.body?.name.replaceAll(' ', '_') 
+        const name = req.body?.name.split(' ').join('_') 
         if (name) {
             const pathFile = path.join(DEST_DOWNLOADS, name + EXTENSION_FILE)
             await unlink(pathFile)
